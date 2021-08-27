2545c9803917299009e7ddcdc767376354890ae5
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-27 17:39:27 -0500

    Implemented routeNotFound and errorHandler

--- src/app.js
+++ src/app.js
@@ -5,11 +5,18 @@ const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
+const routeNotFound = require("./errors/routeNotFound");
+const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
-app.use("/reviews", reviewsRouter);
-app.use("/theaters", theatersRouter);
+
+// Commented out until routes are set up
+// app.use("/reviews", reviewsRouter);
+// app.use("/theaters", theatersRouter);
+
+app.use(routeNotFound);
+app.use(errorHandler);
 
 module.exports = app;
