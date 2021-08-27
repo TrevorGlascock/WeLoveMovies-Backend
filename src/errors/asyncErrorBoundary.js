37b98d7818e4913baf7a82f959a76b6a0ba6b507
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-27 16:09:35 -0500

    Implemented Async Error Boundary higher order wrapper function

---
+++ src/errors/asyncErrorBoundary.js
@@ -0,0 +1,19 @@
+/**
+ * DRY Higher-Order wrapper function to catch any potential async errors.
+ */
+function asyncErrorBoundary(delegate, defaultStatus) {
+    return (request, response, next) => {
+      Promise.resolve()
+        .then(() => delegate(request, response, next))
+        .catch((error = {}) => {
+          const { status = defaultStatus, message = error } = error;
+          next({
+            status,
+            message,
+          });
+        });
+    };
+  }
+  
+  module.exports = asyncErrorBoundary;
+  
