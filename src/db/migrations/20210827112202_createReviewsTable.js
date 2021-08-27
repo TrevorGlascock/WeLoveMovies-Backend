85d03047183fe16a0e5a4a11ff8668474bdd2bc5
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-27 13:17:40 -0500

    fixed incorrect data type

--- src/db/migrations/20210827112202_createReviewsTable.js
+++ src/db/migrations/20210827112202_createReviewsTable.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reviews", (table) => {
     table.increments("review_id").primary();
-    table.string("content");
+    table.text("content");
     table.integer("score");
-    
+
     table.integer("movie_id").unsigned().notNullable();
     table
       .foreign("movie_id")
