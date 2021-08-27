f5d592696f2d9175adc1351e5e83cb51e5785c67
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-27 14:33:06 -0500

    GET requests allowed to '/movies' and '/movies/:movieId', as it is better to just handle the query in the controller

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,13 +2,13 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// GET /movies?is_showing=true = > List of all movies currently found in theaters table
+// GET /movies/:movieId
 router
-  .route("/?is_showing=true")
-  .get(controller.listShowing)
+  .route("/:movieId([0-9]+)")
+  .get(controller.read)
   .all(methodNotAllowed);
 
-// GET /movies => List of all movies
+// GET /movies => List of all movies (may have an is_showing query)
 router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
