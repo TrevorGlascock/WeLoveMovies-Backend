08ce245255ffa7d4a73e0579a0e6a246e82fd9a3
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-27 14:00:59 -0500

    Added reviews router to app

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
 
 module.exports = app;
