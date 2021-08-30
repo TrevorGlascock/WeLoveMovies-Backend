1a930bac2d51840b2a8a38499ed3027cf2ee34b4
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-30 12:19:18 -0500

    Created hasReviewId validation middleware

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,6 +1,19 @@
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+/**************************** Middleware Functions ****************************/
+async function hasReviewId(req, res, next) {
+  const { reviewId } = req.params; // Pull the id from the request parameter
+  const review = await service.read(reviewId); // Let the service find the movie
+
+  // If no review is found, then throw an error
+  if (!review) return next({ status: 404, message: `Review cannot be found.` });
+
+  // Otherwise, set the local variable and proceed through the pipeline
+  res.locals.review = review;
+  return next();
+}
+
 /**************************** CRUDL Operation Handlers ****************************/
 async function list(req, res, next) {
   const data = await service.list();
