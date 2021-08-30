30a2be861ec1de59c55426c6459fca26e7ddc1ab
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-30 14:15:54 -0500

    Created update query

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -2,6 +2,15 @@ const db = require("../db/connection");
 const tableName = "reviews";
 
 /**************************** CRUDL Knex Queries ****************************/
+function update(review) {
+  const { review_id } = review;
+  return db(tableName)
+    .select("*")
+    .where(review_id)
+    .update(review, "*")
+    .then((rows) => rows[0]);
+}
+
 function read(review_id) {
   return db(tableName).select("*").where({ review_id }).first();
 }
@@ -18,4 +27,5 @@ module.exports = {
   list,
   read,
   delete: destroy,
+  update,
 };
