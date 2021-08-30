01df1e9a2ed0de47d5f056b2571eff3c34451f49
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-30 09:50:49 -0500

    First attempt at incorporating is_showing param for join query

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -3,8 +3,15 @@ const tableName = "movies";
 
 /**************************** CRUDL Knex Queries ****************************/
 function list(is_showing = false) {
-  //TODO: need to account for the query
-  return db(tableName).select("*");
+  //This needs testing
+  return is_showing
+    ? // List all currently showing movies
+      db({ m: tableName })
+        .join({ mt: "movies_theaters" }, "mt.movie_id", "m.movie_id")
+        .where({ "mt.is_showing": true })
+        .select("m.*")
+    : // List all Movies
+      db(tableName).select("*");
 }
 
 function read(movie_id) {
