562a4842bd784faf9738a41b483ad3d0a3e56043
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-30 13:26:57 -0500

    added put and delete routes for /reviews/reviewId

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -3,7 +3,11 @@ const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // PUT & DELETE /reviews/:reviewId
-router.route("/:reviewId([0-9]+)").get(controller.read).all(methodNotAllowed);
+router
+  .route("/:reviewId([0-9]+)")
+  .delete(controller.delete)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 // GET /reviews lists all reviews
 router.route("/").get(controller.list).all(methodNotAllowed);
