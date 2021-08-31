9b79196a9d703a9215557a1b16b4ff9c57b08508
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-31 11:19:26 -0500

    using controller.listReviews function instead of importing reviews endpoint

--- src/movies/movieId/movieId.router.js
+++ src/movies/movieId/movieId.router.js
@@ -1,14 +1,13 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./movieId.controller");
 const theatersRouter = require("../../theaters/theaters.router");
-const reviewsRouter = require("../../reviews/reviews.router");
 const methodNotAllowed = require("../../errors/methodNotAllowed");
-
-// GET /movies/:movieId/reviews
-router.use("/reviews", reviewsRouter);
 
 // GET /movies/:movieId/theaters
 router.use("/theaters", theatersRouter);
+
+// GET /movies/:movieId/reviews
+router.route("/reviews").get(controller.listReviews).all(methodNotAllowed);
 
 // GET /movies/:movieId/
 router.route("/").get(controller.read).all(methodNotAllowed);
